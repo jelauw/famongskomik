abade47fdfc8e9fc181f7b3b87c9f5dc5ab98d3e
jelauw <94590792+jelauw@users.noreply.github.com>
2023-05-22 10:28:54 +0700

    Add files via upload

--- js/baca.js
+++ js/baca.js
@@ -45,13 +45,104 @@ window.addEventListener('scroll', function() {
 
 
 
+var navigation = document.getElementById('navigation');
+var isNavVisible = true;
+var timeout;
+
+function hideNavigation() {
+  navigation.classList.add('hidden');
+  isNavVisible = false;
+}
+
+function showNavigation() {
+  navigation.classList.remove('hidden');
+  isNavVisible = true;
+}
+
+window.addEventListener('scroll', function() {
+  if (isNavVisible) {
+    hideNavigation();
+  }
+
+  clearTimeout(timeout);
+  timeout = setTimeout(function() {
+    if (!isNavVisible) {
+      hideNavigation();
+    }
+  }, 300);
+});
+
+document.addEventListener('click', function() {
+  showNavigation();
+});
+
+window.addEventListener('DOMContentLoaded', function() {
+  showNavigation();
+});
+
+window.addEventListener('scroll', function() {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop === 0) {
+    showNavigation();
+  } else {
+    hideNavigation();
+  }
+});
+
+
+
+var fullscreenButton = document.querySelector('a[href="#"]');
+fullscreenButton.addEventListener('click', toggleFullscreen);
+
+function toggleFullscreen() {
+  if (document.fullscreenElement) {
+    exitFullscreen();
+  } else {
+    enterFullscreen();
+  }
+}
+
+function enterFullscreen() {
+  var element = document.documentElement;
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
+
+function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
+
+
+var chapterBtn = document.getElementById('chapterBtn');
+var chapterList = document.getElementById('chapterList');
+
+chapterBtn.addEventListener('click', function() {
+  chapterList.classList.toggle('show');
+});
+
+
 
 
 
 // UPLOAD IMAGE
 var imageContainer = document.getElementById('image-container');
 
-var basePath = 'img-ch/chapter-0001/Gambar '; // Base path atau URL gambar
+var basePath = 'img-ch/Chapter 0001/Gambar '; // Base path atau URL gambar
 var imageCount = 17; // Jumlah gambar
 var imageExtension = '.jpg'; // Ekstensi gambar
 
